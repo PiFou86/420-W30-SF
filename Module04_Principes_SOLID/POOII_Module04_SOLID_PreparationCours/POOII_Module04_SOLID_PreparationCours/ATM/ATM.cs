78de0e9ed95957aa75decd3439c16d8245b6562b
Pierre-François Léon <pierrefrancois.leon@gmail.com>
2021-11-08 23:01:13 -0500

    Ajout exemple Moq

--- Module04_Principes_SOLID/POOII_Module04_SOLID_PreparationCours/POOII_Module04_SOLID_PreparationCours/ATM/ATM.cs
+++ Module04_Principes_SOLID/POOII_Module04_SOLID_PreparationCours/POOII_Module04_SOLID_PreparationCours/ATM/ATM.cs
@@ -1,40 +1,51 @@
-﻿using POOII_Module04_SOLID_PreparationCours.ATM.Comptes;
-using POOII_Module04_SOLID_PreparationCours.ATM.Tiroir;
-using POOII_Module04_SOLID_PreparationCours.ATM.Transactions;
-using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using POOII_Module04_SOLID_PreparationCours.ATM.Comptes;
+using POOII_Module04_SOLID_PreparationCours.ATM.Tiroir;
+using POOII_Module04_SOLID_PreparationCours.ATM.Transactions;
+using System;
+using System.Collections.Generic;
+using System.Text;
 
-namespace POOII_Module04_SOLID_PreparationCours.ATM
+namespace POOII_Module04_SOLID_PreparationCours.ATM
 {
 
-    public class ATM
-    {
-        private ICreateurTransaction m_createurTransaction;
-        private ITiroirArgent m_tiroirArgent;
+    public class ATM
+    {
+        private ICreateurTransaction m_createurTransaction;
+        private ITiroirArgent m_tiroirArgent;
 
-        public ATM(ITiroirArgent p_tiroirArgent, ICreateurTransaction p_createurTransaction)
-        {
-            m_createurTransaction = p_createurTransaction;
-            m_tiroirArgent = p_tiroirArgent;
-        }
+        public ATM(ITiroirArgent p_tiroirArgent, ICreateurTransaction p_createurTransaction)
+        {
+            if (p_tiroirArgent == null)
+            {
+                throw new ArgumentNullException(nameof(p_tiroirArgent));
+            }
+            if (p_createurTransaction == null)
+            {
+                throw new ArgumentNullException(nameof(p_createurTransaction));
+            }
+            m_createurTransaction = p_createurTransaction;
+            m_tiroirArgent = p_tiroirArgent;
+        }
 
-        public void Retirer(ICompte p_compte, decimal p_montant)
-        {
-            ITransaction transaction = m_createurTransaction.CreerTransactionRetirer(p_compte, p_montant);
-            if (transaction.EstValide())
-            {
-                try
-                {
-                    transaction.ExecuterTransaction();
-                    m_tiroirArgent.DistribuerArgent(p_montant);
-                }
-                catch (Exception ex)
-                {
-                    transaction.Annuler();
-                }
+        public void Retirer(ICompte p_compte, decimal p_montant)
+        {
+            if (p_compte is null) { throw new ArgumentNullException(nameof(p_compte)); }
+            if (p_montant <= 0) { throw new ArgumentException("Le montant doit être supérieur à 0", nameof(p_montant)); }
 
-            }
-        }
-    }
+            ITransaction transaction = m_createurTransaction.CreerTransactionRetirer(p_compte, p_montant);
+            if (transaction.EstValide())
+            {
+                try
+                {
+                    transaction.ExecuterTransaction();
+                    m_tiroirArgent.DistribuerArgent(p_montant);
+                }
+                catch (Exception ex)
+                {
+                    transaction.Annuler();
+                }
+
+            }
+        }
+    }
 }
