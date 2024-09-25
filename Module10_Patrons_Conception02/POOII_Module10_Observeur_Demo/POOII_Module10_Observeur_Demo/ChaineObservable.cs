7f84e543296c02f4fb184a1cfa35194adefe1fb4
Jean-Christophe Couture <coujean99@hotmail.com>
2024-09-24 21:17:25 -0400

    Module 10 correction de l'encodage, passage à .NET 8.0, ajustement des namespaces

--- Module10_Patrons_Conception02/POOII_Module10_Observeur_Demo/POOII_Module10_Observeur_Demo/ChaineObservable.cs
+++ Module10_Patrons_Conception02/POOII_Module10_Observeur_Demo/POOII_Module10_Observeur_Demo/ChaineObservable.cs
@@ -1,52 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System;
+using System.Collections.Generic;
 
-namespace POOII_Module10_Observeur_Demo
+namespace POOII_Module10_Observeur_Demo;
+
+// Sujet
+public class ChaineObservable : IObservable<string>
 {
-    // Sujet
-    public class ChaineObservable : IObservable<string>
-    {
-        private List<IObserver<string>> m_observeurs;
-        private string m_valeur;
-        public ChaineObservable()
-        {
-            this.m_observeurs = new List<IObserver<string>>();
-        }
+    private List<IObserver<string>> m_observeurs;
+    private string m_valeur;
+    public ChaineObservable()
+    {
+        this.m_observeurs = new List<IObserver<string>>();
+    }
 
-        public string Valeur
-        {
-            get
-            {
-                return this.m_valeur;
-            }
-            set
-            {
-                this.m_valeur = value;
-                this.InformerObersateurValeurChangee();
-            }
-        }
+    public string Valeur
+    {
+        get
+        {
+            return this.m_valeur;
+        }
+        set
+        {
+            this.m_valeur = value;
+            this.InformerObersateurValeurChangee();
+        }
+    }
 
-        private void InformerObersateurValeurChangee()
-        {
-            this.m_observeurs.ForEach(observer =>
-                observer.OnNext(this.Valeur)
-            );
-        }
+    private void InformerObersateurValeurChangee()
+    {
+        this.m_observeurs.ForEach(observer =>
+        observer.OnNext(this.Valeur)
+        );
+    }
 
-        // PFL : attach
-        public IDisposable Subscribe(IObserver<string> p_observateur)
-        {
-            if (p_observateur is null)
-            {
-                throw new ArgumentNullException(nameof(p_observateur));
-            }
+    // PFL : attach
+    public IDisposable Subscribe(IObserver<string> p_observateur)
+    {
+        if (p_observateur is null)
+        {
+            throw new ArgumentNullException(nameof(p_observateur));
+        }
 
-            this.m_observeurs.Add(p_observateur);
+        this.m_observeurs.Add(p_observateur);
 
-            return new UnsubscribeChaineObservable(this.m_observeurs, p_observateur);
-        }
-
-
-    }
+        return new UnsubscribeChaineObservable(this.m_observeurs, p_observateur);
+    }
 }
--- Module10_Patrons_Conception02/POOII_Module10_Observeur_Demo/POOII_Module10_Observeur_Demo/ChaineObservateurGenerique.cs
+++ Module10_Patrons_Conception02/POOII_Module10_Observeur_Demo/POOII_Module10_Observeur_Demo/ChaineObservateurGenerique.cs
@@ -1,50 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System;
 
-namespace POOII_Module10_Observeur_Demo
+namespace POOII_Module10_Observeur_Demo;
+
+public class ChaineObservateurGenerique : IObserver<string>
 {
-    public class ChaineObservateurGenerique : IObserver<string>
-    {
-        private IDisposable _seDesabonner;
-        private Action<string> _action;
+    private IDisposable _seDesabonner;
+    private Action<string> _action;
 
-        public ChaineObservateurGenerique(IObservable<string> p_sujet, Action<string> p_action)
-        {
-            if (p_sujet is null)
-            {
-                throw new ArgumentNullException(nameof(p_sujet));
-            }
+    public ChaineObservateurGenerique(IObservable<string> p_sujet, Action<string> p_action)
+    {
+        if (p_sujet is null)
+        {
+            throw new ArgumentNullException(nameof(p_sujet));
+        }
 
-            if (p_action is null)
-            {
-                throw new ArgumentNullException(nameof(p_action));
-            }
+        if (p_action is null)
+        {
+            throw new ArgumentNullException(nameof(p_action));
+        }
 
-            this._action = p_action;
-            this._seDesabonner = p_sujet.Subscribe(this);
-        }
+        this._action = p_action;
+        this._seDesabonner = p_sujet.Subscribe(this);
+    }
 
-        public void SeDesabonner()
-        {
-            this._seDesabonner?.Dispose();
-            this._seDesabonner = null;
-        }
+    public void SeDesabonner()
+    {
+        this._seDesabonner?.Dispose();
+        this._seDesabonner = null;
+    }
 
-        public void OnCompleted()
-        {
-            this.SeDesabonner();
-        }
+    public void OnCompleted()
+    {
+        this.SeDesabonner();
+    }
 
-        public void OnError(Exception error)
-        {
-            throw new NotImplementedException();
-        }
+    public void OnError(Exception error)
+    {
+        throw new NotImplementedException();
+    }
 
-        // PFL : update
-        public void OnNext(string p_valeur)
-        {
-            this._action(p_valeur);
-        }
-    }
+    // PFL : update
+    public void OnNext(string p_valeur)
+    {
+        this._action(p_valeur);
+    }
 }
