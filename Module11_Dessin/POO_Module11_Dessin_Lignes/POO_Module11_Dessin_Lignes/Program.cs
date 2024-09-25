22a8d8546d14029930e89235c0dcc67b5b9311c0
Jean-Christophe Couture <coujean99@hotmail.com>
2024-09-24 21:20:05 -0400

    Module 11 correction de l'encodage, passage à .NET 8.0, ajustement des namespaces

--- Module11_Dessin/POO_Module11_Dessin_Lignes/POO_Module11_Dessin_Lignes/Program.cs
+++ Module11_Dessin/POO_Module11_Dessin_Lignes/POO_Module11_Dessin_Lignes/Program.cs
@@ -1,23 +1,19 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+using System;
+using System.Windows.Forms;
 
-namespace POO_Module11_Dessin_Lignes
+namespace POO_Module11_Dessin_Lignes;
+
+static class Program
 {
-    static class Program
-    {
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
-        {
-            Application.SetHighDpiMode(HighDpiMode.SystemAware);
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new fDessinLignes());
-        }
-    }
+    /// <summary>
+            ///  The main entry point for the application.
+            /// </summary>
+    [STAThread]
+    static void Main()
+    {
+        Application.SetHighDpiMode(HighDpiMode.SystemAware);
+        Application.EnableVisualStyles();
+        Application.SetCompatibleTextRenderingDefault(false);
+        Application.Run(new fDessinLignes());
+    }
 }
--- Module11_Dessin/POO_Module11_Dessin_Lignes/POO_Module11_Dessin_Lignes/fDessinLignes.cs
+++ Module11_Dessin/POO_Module11_Dessin_Lignes/POO_Module11_Dessin_Lignes/fDessinLignes.cs
@@ -1,97 +1,89 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace POO_Module11_Dessin_Lignes
+namespace POO_Module11_Dessin_Lignes;
+
+public partial class fDessinLignes : Form
 {
-    public partial class fDessinLignes : Form
+    public fDessinLignes()
     {
-        public fDessinLignes()
+        InitializeComponent();
+    }
+
+    public ConfigurationRevolution EtablirConfiguration()
+    {
+        ConfigurationRevolution cr = new ConfigurationRevolution()
         {
-            InitializeComponent();
+            PasAngle = (int)nudPasAngle.Value,
+            LongueurDepart = (int)nudLongueurDepart.Value,
+            NombreLignes = (int)nudNombreLignes.Value,
+            PasLongueur = (int)nudPasLongueur.Value,
+            PositionDepart = new Point(pCanvas.Width / 2, pCanvas.Height / 2),
+            NombreIterationKock = (int)nudNombreIterationsFloconsKoch.Value
+        };
+
+        return cr;
+    }
+
+    private void Dessiner(ConfigurationRevolution p_configurationRevolution, Graphics p_graphics, Pen p_pen)
+    {
+        if (p_configurationRevolution is null)
+        {
+            throw new ArgumentNullException(nameof(p_configurationRevolution));
         }
 
-        public ConfigurationRevolution EtablirConfiguration()
+        if (p_graphics is null)
         {
-            ConfigurationRevolution cr = new ConfigurationRevolution()
-            {
-                PasAngle = (int)nudPasAngle.Value,
-                LongueurDepart = (int)nudLongueurDepart.Value,
-                NombreLignes = (int)nudNombreLignes.Value,
-                PasLongueur = (int)nudPasLongueur.Value,
-                PositionDepart = new Point(pCanvas.Width / 2, pCanvas.Height / 2),
-                NombreIterationKock = (int)nudNombreIterationsFloconsKoch.Value
-            };
-
-            return cr;
+            throw new ArgumentNullException(nameof(p_graphics));
         }
 
-        private void Dessiner(ConfigurationRevolution p_configurationRevolution, Graphics p_graphics, Pen p_pen)
+        List<PointF> pointsLignes = DessinRevolution.GenererPoints(p_configurationRevolution);
+
+        p_graphics.DrawLines(p_pen, pointsLignes.ToArray());
+    }
+
+    private void DessinerKoch(ConfigurationRevolution p_configurationRevolution, Graphics p_graphics, Pen p_pen)
+    {
+        if (p_configurationRevolution is null)
         {
-            if (p_configurationRevolution is null)
-            {
-                throw new ArgumentNullException(nameof(p_configurationRevolution));
-            }
-
-            if (p_graphics is null)
-            {
-                throw new ArgumentNullException(nameof(p_graphics));
-            }
-
-            List<PointF> pointsLignes = DessinRevolution.GenererPoints(p_configurationRevolution);
-
-            p_graphics.DrawLines(p_pen, pointsLignes.ToArray());
+            throw new ArgumentNullException(nameof(p_configurationRevolution));
         }
 
-        private void DessinerKoch(ConfigurationRevolution p_configurationRevolution, Graphics p_graphics, Pen p_pen)
+        if (p_graphics is null)
         {
-            if (p_configurationRevolution is null)
-            {
-                throw new ArgumentNullException(nameof(p_configurationRevolution));
-            }
-
-            if (p_graphics is null)
-            {
-                throw new ArgumentNullException(nameof(p_graphics));
-            }
-
-            List<PointF> pointsLignes = DessinRevolution.GenererPoints(p_configurationRevolution);
-            pointsLignes = DessinRevolution.SubDiviserKock(pointsLignes, p_configurationRevolution.NombreIterationKock);
-            pointsLignes = DessinRevolution.Recentrer(pointsLignes, pCanvas.Width, pCanvas.Height);
-
-            p_graphics.DrawLines(p_pen, pointsLignes.ToArray());
+            throw new ArgumentNullException(nameof(p_graphics));
         }
 
-        private void bDessiner_Click(object sender, EventArgs e)
+        List<PointF> pointsLignes = DessinRevolution.GenererPoints(p_configurationRevolution);
+        pointsLignes = DessinRevolution.SubDiviserKock(pointsLignes, p_configurationRevolution.NombreIterationKock);
+        pointsLignes = DessinRevolution.Recentrer(pointsLignes, pCanvas.Width, pCanvas.Height);
+
+        p_graphics.DrawLines(p_pen, pointsLignes.ToArray());
+    }
+
+    private void bDessiner_Click(object sender, EventArgs e)
+    {
+        using (Graphics graphics = pCanvas.CreateGraphics())
         {
-            using (Graphics graphics = pCanvas.CreateGraphics())
+            graphics.Clear(Color.White);
+            using (Pen pen = new Pen(Color.Black))
             {
-                graphics.Clear(Color.White);
-                using (Pen pen = new Pen(Color.Black))
-                {
-                    Dessiner(this.EtablirConfiguration(), graphics, pen);
-                }
+                Dessiner(this.EtablirConfiguration(), graphics, pen);
             }
         }
+    }
 
-        private void bSubdiviserFloconsKoch_Click(object sender, EventArgs e)
+    private void bSubdiviserFloconsKoch_Click(object sender, EventArgs e)
+    {
+        using (Graphics graphics = pCanvas.CreateGraphics())
         {
-            using (Graphics graphics = pCanvas.CreateGraphics())
+            graphics.Clear(Color.White);
+            using (Pen pen = new Pen(Color.Black))
             {
-                graphics.Clear(Color.White);
-                using (Pen pen = new Pen(Color.Black))
-                {
-                    DessinerKoch(this.EtablirConfiguration(), graphics, pen);
-                }
+                DessinerKoch(this.EtablirConfiguration(), graphics, pen);
             }
         }
-
-        
     }
 }
