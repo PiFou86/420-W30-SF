baa8ddac1fe638b3c1fdd41bc99545fe7da0ac29
Jean-Christophe Couture <coujean99@hotmail.com>
2024-09-24 21:06:15 -0400

    Module 8 maj des paquets, correction de l'encodage (sauf ClientUIConsole), passage à .NET 8.0, ajustement des namespaces

--- Module08_ArchitectureDesApplications/Module08_Exercice01_Base/Module08_Exercice01_Base_Console/CoucheAccesDonnees/XML/DTO/ClientXMLDTO.cs
+++ Module08_ArchitectureDesApplications/Module08_Exercice01_Base/Module08_Exercice01_Base_Console/CoucheAccesDonnees/XML/DTO/ClientXMLDTO.cs
@@ -1,35 +1,33 @@
-﻿using Module08_Exercice01_Base_Console.Entites;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Xml.Serialization;
+﻿using Module08_Exercice01_Base_Console.Entites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Serialization;
 
-namespace Module08_Exercice01_Base_Console.CoucheAccesDonnees.XML.DTO
+namespace Module08_Exercice01_Base_Console.CoucheAccesDonnees.XML.DTO;
+
+[XmlType("Client")]
+public class ClientXMLDTO
 {
-    [XmlType("Client")]
-    public class ClientXMLDTO
-    {
-        public ClientXMLDTO() { }
+    public ClientXMLDTO() { }
 
-        public ClientXMLDTO(Client p_client)
-        {
-            this.ClientId = p_client.ClientId;
-            this.Nom = p_client.Nom;
-            this.Prenom = p_client.Prenom;
-            this.Adresses = p_client.Adresses?.Select(a => new AdresseXMLDTO(a)).ToList();
-        }
+    public ClientXMLDTO(Client p_client)
+    {
+        this.ClientId = p_client.ClientId;
+        this.Nom = p_client.Nom;
+        this.Prenom = p_client.Prenom;
+        this.Adresses = p_client.Adresses?.Select(a => new AdresseXMLDTO(a)).ToList();
+    }
 
-        public Guid ClientId { get; set; }
-        public string Nom { get; set; }
-        public string Prenom { get; set; }
-        public List<AdresseXMLDTO> Adresses { get; set; }
+    public Guid ClientId { get; set; }
+    public string Nom { get; set; }
+    public string Prenom { get; set; }
+    public List<AdresseXMLDTO> Adresses { get; set; }
 
-        public Client VersEntite()
-        {
-            List<Adresse> adresses = this.Adresses.Select(aDTO => aDTO.VersEntite()).ToList();
+    public Client VersEntite()
+    {
+        List<Adresse> adresses = this.Adresses.Select(aDTO => aDTO.VersEntite()).ToList();
 
-            return new Client(this.ClientId, this.Nom, this.Prenom, adresses);
-        }
-    }
+        return new Client(this.ClientId, this.Nom, this.Prenom, adresses);
+    }
 }
