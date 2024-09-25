8c26556cbadf6cbf26763469b390a8688422a9ca
Jean-Christophe Couture <coujean99@hotmail.com>
2024-09-24 20:22:45 -0400

    Module 5 correction de l'encodage (sauf Program.cs), passage à .NET 8.0, ajustement des namespaces

--- Module05_Demeter_TellDontAsk/POOII_Module05_Demeter_TellDontAsk_Console/POOII_Module05_Demeter_TellDontAsk_Console/Cercle.cs
+++ Module05_Demeter_TellDontAsk/POOII_Module05_Demeter_TellDontAsk_Console/POOII_Module05_Demeter_TellDontAsk_Console/Cercle.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿namespace POOII_Module05_Demeter_TellDontAsk_Console;
 
-namespace POOII_Module05_Demeter_TellDontAsk_Console
+public class Cercle
 {
-    public class Cercle
-    {
-        public Point3d Centre { get; set; }
-        public float Rayon { get; set; }
-    }
+    public Point3d Centre { get; set; }
+    public float Rayon { get; set; }
 }
